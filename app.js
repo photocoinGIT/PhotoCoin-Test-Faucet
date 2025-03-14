c5798691b67028e8de6320e3d14cdd4b9c98ec35
photocoinGIT <photocoinforx@gmail.com>
2025-03-14 21:45:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -46,69 +46,95 @@ async function getIP() {
     
     const faucetAddress = document.querySelector('meta[name="contract-address"]').content;
     const faucetAbi = [
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "tokenAddress",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-        },
-        {
-            "inputs": [],
-            "name": "claim",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "inputs": [],
-            "name": "claimAmount",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "name": "lastClaim",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [],
-            "name": "token",
-            "outputs": [
-                {
-                    "internalType": "contract IERC20",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        }
+    	{
+    		"inputs": [],
+    		"name": "claim",
+    		"outputs": [],
+    		"stateMutability": "nonpayable",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [
+    			{
+    				"internalType": "uint256",
+    				"name": "newAmount",
+    				"type": "uint256"
+    			}
+    		],
+    		"name": "setClaimAmount",
+    		"outputs": [],
+    		"stateMutability": "nonpayable",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [
+    			{
+    				"internalType": "address",
+    				"name": "tokenAddress",
+    				"type": "address"
+    			}
+    		],
+    		"stateMutability": "nonpayable",
+    		"type": "constructor"
+    	},
+    	{
+    		"inputs": [],
+    		"name": "claimAmount",
+    		"outputs": [
+    			{
+    				"internalType": "uint256",
+    				"name": "",
+    				"type": "uint256"
+    			}
+    		],
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [
+    			{
+    				"internalType": "address",
+    				"name": "",
+    				"type": "address"
+    			}
+    		],
+    		"name": "lastClaim",
+    		"outputs": [
+    			{
+    				"internalType": "uint256",
+    				"name": "",
+    				"type": "uint256"
+    			}
+    		],
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [],
+    		"name": "owner",
+    		"outputs": [
+    			{
+    				"internalType": "address",
+    				"name": "",
+    				"type": "address"
+    			}
+    		],
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [],
+    		"name": "token",
+    		"outputs": [
+    			{
+    				"internalType": "contract IERC20",
+    				"name": "",
+    				"type": "address"
+    			}
+    		],
+    		"stateMutability": "view",
+    		"type": "function"
+    	}
     ];
     
     const faucetContract = new ethers.Contract(faucetAddress, faucetAbi, signer);
